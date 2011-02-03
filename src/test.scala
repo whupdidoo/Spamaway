ef6667b14fe6103e7e640a67011c5950db579b63
Rene <noodleboy@web.de>
2011-02-03 21:20:24 +0100

    test2 fertig, aber class not found exception kommt

--- src/test.scala
+++ src/test.scala
@@ -1,7 +1,10 @@
 import edu.berkeley.compbio.jlibsvm.binary._
 import edu.berkeley.compbio.jlibsvm.kernel._
 import edu.berkeley.compbio.jlibsvm._
+import edu.berkeley.compbio.jlibsvm.ImmutableSvmParameterPoint.Builder
 import edu.berkeley.compbio.jlibsvm.util.SparseVector;
+
+import scala.runtime.RichFloat
 
 import java.util.HashSet
 
@@ -9,22 +12,29 @@ object test
 {
 	def main(args: Array[String])
 	{
-		var labels = Array("SPAM", "HAM")
+		var classes = Array("SPAM", "HAM")
 		// SparseVector gave me no errors, so i kept it. SparseVector is a java class from jlibsvm
-		var train1 = new SparseVector(2)
-		var train2 = new SparseVector(2)
-		train1.indexes = Array(0, 1)
-		train1.values = Array(0.1f, 0.3f)
-		train1.indexes = Array(0, 1)
-		train1.values = Array(0.8f, 0.5f)
+		var numClasses = classes.length
+		var numTrainVectors = 100
+		var numDimensions = 2
+		var trainVectors : Array[SparseVector] = new Array(numTrainVectors);
+		var trainVectorClasses : Array[String] = new Array(numTrainVectors);
+		var problem = new MutableBinaryClassificationProblemImpl[String, SparseVector](classes.getClass, numClasses)
+		for(i <- 0 to numTrainVectors-1)
+		{
+			var currTrainVector = trainVectors(i);
+			currTrainVector = new SparseVector(numDimensions)
+			currTrainVector.indexes = (0 until numTrainVectors-1).toArray
+			// every second item is of the same class
+			trainVectorClasses(i) = classes(i % 2);
+			currTrainVector.values = Array.fill(numDimensions){ var ran = java.lang.Math.random; ran.floatValue }
+			var offset = (i % 2) * 0.9f;
+			currTrainVector.values(0) += offset	// so that classes are separable, with 10% overlap
+			problem.addExample(currTrainVector, trainVectorClasses(i))
+		}
 		
-		var trainDataElement = Array(train1, train2);
-		
-		var problem = new MutableBinaryClassificationProblemImpl[String, SparseVector](labels.getClass, 2)
-		// this is a for-loop in scala, not uncool
-		(0 until 1).foreach( i => problem.addExample(trainDataElement(i), labels(i)) )
-		//problem.setupLabels();	// why does this throw an IndexOutOfBoundsException?		var svm = new C_SVC[String,SparseVector]
-		var builder : ImmutableSvmParameterGrid.Builder[String, SparseVector] = ImmutableSvmParameterGrid.builder()
+		//problem.setupLabels();
+		var builder : ImmutableSvmParameterPoint.Builder[String, SparseVector] = new ImmutableSvmParameterPoint.Builder
 		builder.nu = 0.5f;
 		builder.cache_size = 100;
 		builder.eps = 1e-3f;
@@ -32,13 +42,17 @@ object test
 		builder.shrinking = true;
 		builder.probability = false;
 		builder.redistributeUnbalancedC = true;
-		builder.kernelSet = new HashSet()
-		builder.kernelSet.add(new LinearKernel())	// others are also possible
-		var cSet = List(1.0f,2.0f);
-		builder.Cset = new java.util.ArrayList()
-		cSet.foreach(el => builder.Cset.add(el))	// copy scala list to java list
+		//builder.kernelSet = new HashSet()
+		//builder.kernelSet.add(new LinearKernel())	// others are also possible
+		builder.kernel = new LinearKernel()
+		builder.C = 1.0f;
+		//var cSet = List(1.0f);
+		//builder.Cset = new java.util.ArrayList()
+		//cSet.foreach(el => builder.Cset.add(el))	// copy scala list to java list
 		var param = builder.build
 		var svm = new C_SVC[String, SparseVector]	// also possible: Nu_SVC, EpsilonSVR, Nu_SVR
-		var model = svm.train(problem, param)
+		svm.validateParam(param)
+		//var model = svm.train(problem, param)
+		var model = svm.trainOne(problem, 1.0f, 1.0f, param)
 	}
 }
---
+++ src/test2.scala
@@ -0,0 +1,91 @@
+import libsvm._
+
+import java.io._
+import java.util._
+
+class test2 {
+	def main(args: Array[String])
+	{
+		var classes = Array("SPAM", "NOSPAM")
+		// SparseVector gave me no errors, so i kept it. SparseVector is a java class from jlibsvm
+		var numClasses = classes.length
+		var numTrainVectors = 100
+		var numDimensions = 2
+		var trainVectors : Array[Array[svm_node]] = new Array(numTrainVectors);
+		var trainVectorClasses : Array[Double] = new Array(numTrainVectors);
+		//var problem = new MutableBinaryClassificationProblemImpl[String, SparseVector](classes.getClass, numClasses)
+		
+		for(i <- 0 to numTrainVectors-1)
+		{
+			var currTrainVector = trainVectors(i);
+			currTrainVector = new Array[svm_node](numDimensions)
+			//currTrainVector.indexes = (0 until numTrainVectors-1).toArray
+			// every second item is of the same class
+			trainVectorClasses(i) = i % 2;
+			//currTrainVector = Array.fill(numDimensions){ var ran = java.lang.Math.random; var node = new svm_node; node.index ran.floatValue }
+			for (j <- 0 until numDimensions-1){
+				var ran = java.lang.Math.random
+				var node = new svm_node
+				node.index = j
+				node.value = ran.floatValue
+				currTrainVector(j) = node
+			}
+			var offset = (i % 2) * 0.9f;
+			currTrainVector(0).value += offset	// so that classes are separable, with 10% overlap
+			//problem.addExample(currTrainVector, trainVectorClasses(i))
+		}
+
+		var prob = new svm_problem()
+		prob.l = numTrainVectors
+		prob.x = trainVectors
+		prob.y = trainVectorClasses
+
+		
+		//problem.setupLabels();
+/*		var builder : ImmutableSvmParameterPoint.Builder[String, SparseVector] = new ImmutableSvmParameterPoint.Builder
+		builder.nu = 0.5f;
+		builder.cache_size = 100;
+		builder.eps = 1e-3f;
+		builder.p = 0.1f;
+		builder.shrinking = true;
+		builder.probability = false;
+		builder.redistributeUnbalancedC = true;
+		//builder.kernelSet = new HashSet()
+		//builder.kernelSet.add(new LinearKernel())	// others are also possible
+		builder.kernel = new LinearKernel()
+		builder.C = 1.0f;
+		//var cSet = List(1.0f);
+		//builder.Cset = new java.util.ArrayList()
+		//cSet.foreach(el => builder.Cset.add(el))	// copy scala list to java list
+		var param = builder.build*/
+		var param : svm_parameter = new svm_parameter
+		// default values
+		param.svm_type = svm_parameter.C_SVC	// needs no params as i see it...
+		param.kernel_type = svm_parameter.LINEAR;	//0 -- linear: u'*v 1 -- polynomial: (gamma*u'*v + coef0)^degree	2 -- radial basis function: exp(-gamma*|u-v|^2)	3 -- sigmoid: tanh(gamma*u'*v + coef0)
+		param.degree = 3;
+		param.gamma = 0;	// 1/num_features
+			param.gamma = 1.0 / numDimensions;	// width of rbf
+		param.coef0 = 0;
+		param.nu = 0.5;
+		param.cache_size = 100;
+		param.C = 1;
+		param.eps = 1e-3;
+		param.p = 0.1;
+		param.shrinking = 1;
+		param.probability = 0;
+		param.nr_weight = 0;
+		param.weight_label = new Array[Int](0)
+		param.weight = new Array[Double](0)
+		param.kernel_type = svm_parameter.LINEAR
+		
+		var error_msg = svm.svm_check_parameter(prob,param);
+		println(error_msg)
+		if(error_msg.equals(null))
+		{
+			println("ParamCheckError: "+error_msg+"\n");
+		}
+		
+		var model = svm.svm_train(prob,param);
+		//svm.svm_save_model(model_file_name,model);
+	}
+}
